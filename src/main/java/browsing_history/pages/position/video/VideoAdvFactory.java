1b8daa944b00319af9232cfeb755a1a7ef8ba36f
ArtorNado <49398706+ArtorNado@users.noreply.github.com>
2020-03-17 20:20:00 +0300

    Update VideoAdvFactory.java

--- src/main/java/browsing_history/pages/position/video/VideoAdvFactory.java
+++ src/main/java/browsing_history/pages/position/video/VideoAdvFactory.java
@@ -3,9 +3,6 @@ package browsing_history.pages.position.video;
 import browsing_history.pages.page.Page;
 import browsing_history.pages.position.Position;
 import browsing_history.pages.position.PositionFactory;
-import ru.naumow.pages.page.Page;
-import ru.naumow.pages.position.Position;
-import ru.naumow.pages.position.PositionFactory;
 
 public class VideoAdvFactory implements PositionFactory {
     @Override
